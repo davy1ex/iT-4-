7c878892c3774d0bad223be5f755db8e95aab2bd
davy1ex <davy1ex@yandex.ru>
2024-03-25 13:13:24 +0400

    f

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def handle_post():
+    data = request.get_json()
+    
+    if data is None:
+        print("No JSON received")
+        return jsonify({"error": "No JSON received"}), 400
+    print(json.dumps(data, indent=4))  # Pretty print the received JSON data
+    return jsonify({"message": "Data received"}), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=12345)
